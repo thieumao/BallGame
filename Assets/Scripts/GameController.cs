b62cf42e843a481085d88958cc05a91b5291205e
Thieu Mao <thieumao@gmail.com>
2020-10-19 00:10:52 +0700

    Finish game

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -11,9 +11,13 @@ public class GameController : MonoBehaviour
     int m_score;
     bool m_isGameover;
 
+    UIManager m_ui;
+
     // Start is called before the first frame update
     void Start() {
         m_spawnTime = 0;
+        m_ui = FindObjectOfType<UIManager>();
+        m_ui.SetScoreText("Score: " + 0);
     }
 
     // Update is called once per frame
@@ -22,6 +26,7 @@ public class GameController : MonoBehaviour
 
         if (m_isGameover) {
             m_spawnTime = 0;
+            m_ui.ShowGameoverPanel(true);
             return;
         }
 
@@ -38,6 +43,13 @@ public class GameController : MonoBehaviour
         }
     }
 
+    public void Replay() {
+        m_score = 0;
+        m_isGameover = false;
+        m_ui.SetScoreText("Score: " + m_score);
+        m_ui.ShowGameoverPanel(false);
+    }
+
     public void SetScore(int value) {
         m_score = value;
     }
@@ -48,6 +60,7 @@ public class GameController : MonoBehaviour
 
     public void IncrementScore() {
         m_score++;
+        m_ui.SetScoreText("Score: " + m_score);
     }
 
     public bool IsGameover() {
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+    public Text scoreText;
+
+    public GameObject gameoverPanel;
+
+    public void SetScoreText(string txt) {
+        if (scoreText) {
+            scoreText.text = txt;
+        }
+    }
+
+    public void ShowGameoverPanel(bool isShow) {
+        if (gameoverPanel) {
+            gameoverPanel.SetActive(isShow);
+        }
+    }
+
+    void Start() {
+        scoreText.color = Color.red;
+    }
+}
