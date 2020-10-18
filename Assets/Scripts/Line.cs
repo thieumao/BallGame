086c3bc65af80eb309421e241e9fa0f9577815cb
Thieu Mao <thieumao@gmail.com>
2020-10-18 14:23:04 +0700

    Init Project

---
+++ Assets/Scripts/Line.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line : MonoBehaviour
+{
+    public float moveSpeed;
+    float xDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xDirection = Input.GetAxisRaw("Horizontal");
+        float moveStep;
+        if (xDirection != 0)
+        {
+            moveStep = moveSpeed * xDirection * Time.deltaTime;
+            transform.position = transform.position + new Vector3(moveStep, 0, 0);
+            Debug.Log("Luc am");
+            Debug.Log(moveStep);
+        } 
+    }
+}
